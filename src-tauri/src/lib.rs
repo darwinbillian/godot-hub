869819b23034049bac278d21f0d718e4fe1e377b
Darwin Billiano <darwinbillian@gmail.com>
2026-06-08 17:23:14 +0700

    feat: add middleware for caching HTTP responses

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,16 +1,20 @@
+use http_cache_reqwest::{CACacheManager, Cache, CacheMode, HttpCache, HttpCacheOptions};
 use reqwest::Client;
+use reqwest_middleware::{ClientBuilder, ClientWithMiddleware};
 use serde::{Deserialize, Serialize};
-use tauri::State;
+use tauri::{Manager, State};
 use thiserror::Error;
 
 struct AppState {
-    client: Client,
+    client: ClientWithMiddleware,
 }
 
 #[derive(Error, Debug)]
 enum Error {
     #[error(transparent)]
     Reqwest(#[from] reqwest::Error),
+    #[error(transparent)]
+    ReqwestMiddleware(#[from] reqwest_middleware::Error),
     #[error(transparent)]
     Yaml(#[from] yaml_serde::Error),
 }
@@ -56,8 +60,20 @@ async fn list_versions(state: State<'_, AppState>) -> Result<Vec<Version>, Error
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .manage(AppState {
-            client: Client::new(),
+        .setup(|app| {
+            app.manage(AppState {
+                client: ClientBuilder::new(Client::new())
+                    .with(Cache(HttpCache {
+                        mode: CacheMode::Default,
+                        manager: CACacheManager::new(
+                            app.path().app_local_data_dir()?.join("cache"),
+                            false,
+                        ),
+                        options: HttpCacheOptions::default(),
+                    }))
+                    .build(),
+            });
+            Ok(())
         })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![list_versions])
