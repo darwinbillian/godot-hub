56a57539014a9ecf07bee0631a502dc99e183976
Darwin Billiano <darwinbillian@outlook.com>
2026-06-27 20:45:34 +0700

    refactor: add btn, card, list and, menu classes

--- src/pages/installs/InstallListPage.tsx
+++ src/pages/installs/InstallListPage.tsx
@@ -45,18 +45,19 @@ export default function InstallListPage() {
           <h1 className="text-2xl font-semibold">Installs</h1>
         </div>
         <div>
-          <Link
-            className="cursor-pointer rounded bg-blue-500 px-2 py-1 font-semibold transition hover:bg-blue-600"
-            to="/installs/new"
-          >
+          <Link className="btn btn-primary" to="/installs/new">
             Install Editor
           </Link>
         </div>
       </div>
-      <div className="flex flex-col gap-4">
-        {installs?.map((install) => (
-          <InstallCard key={install.id} install={install} />
-        ))}
+      <div>
+        <ul className="list gap-4">
+          {installs?.map((install) => (
+            <li key={install.id}>
+              <InstallCard install={install} />
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
@@ -64,7 +65,7 @@ export default function InstallListPage() {
 
 function InstallCard({ install }: { install: Install }) {
   return (
-    <div className="flex gap-2 rounded border border-white/10 bg-neutral-800 p-4">
+    <div className="card flex gap-2 p-4">
       <div className="flex flex-1 gap-2">
         <img className="size-8" src="/icon.svg" />
         <div>
@@ -85,7 +86,7 @@ function InstallButton({ install }: { install: Install }) {
   return (
     <div className="relative flex items-stretch">
       <button
-        className="flex cursor-pointer items-center gap-1 rounded-l bg-blue-500 px-2 py-1 font-semibold transition hover:bg-blue-600"
+        className="btn btn-primary rounded-r-none"
         onClick={() => {
           launch(install.id).catch((e) => console.error(e));
         }}
@@ -94,7 +95,7 @@ function InstallButton({ install }: { install: Install }) {
         Launch
       </button>
       <button
-        className="cursor-pointer rounded-r bg-blue-500 p-1 transition hover:bg-blue-600"
+        className="btn btn-primary rounded-l-none p-1"
         onClick={() => {
           setExpand((expand) => !expand);
         }}
@@ -109,29 +110,33 @@ function InstallButton({ install }: { install: Install }) {
               setExpand(false);
             }}
           />
-          <div className="absolute top-full right-0 z-10 flex w-max flex-col rounded border border-white/10 bg-neutral-800 p-2">
-            <button
-              className="flex cursor-pointer items-center gap-2 rounded px-2 py-1 transition hover:bg-white/10"
-              onClick={() => {
-                reveal(install.id).catch((e) => console.error(e));
+          <div className="absolute top-full right-0 z-10 w-max">
+            <ul className="menu">
+              <li>
+                <button
+                  onClick={() => {
+                    reveal(install.id).catch((e) => console.error(e));
 
-                setExpand(false);
-              }}
-            >
-              <FolderOpenIcon size={16} />
-              Show in Explorer
-            </button>
-            <button
-              className="flex cursor-pointer items-center gap-2 rounded px-2 py-1 transition hover:bg-white/10"
-              onClick={() => {
-                uninstall(install.id).catch((e) => console.error(e));
+                    setExpand(false);
+                  }}
+                >
+                  <FolderOpenIcon size={16} />
+                  Show in Explorer
+                </button>
+              </li>
+              <li>
+                <button
+                  onClick={() => {
+                    uninstall(install.id).catch((e) => console.error(e));
 
-                setExpand(false);
-              }}
-            >
-              <Trash2Icon size={16} />
-              Uninstall
-            </button>
+                    setExpand(false);
+                  }}
+                >
+                  <Trash2Icon size={16} />
+                  Uninstall
+                </button>
+              </li>
+            </ul>
           </div>
         </>
       )}
