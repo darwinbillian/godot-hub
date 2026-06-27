42bf0d23cec6deaf0490b1e2a586d35db846fb80
Darwin Billiano <darwinbillian@outlook.com>
2026-06-27 20:45:34 +0700

    refactor: add btn, card, list and, menu classes

--- src/pages/installs/NewInstallPage.tsx
+++ src/pages/installs/NewInstallPage.tsx
@@ -15,18 +15,19 @@ export default function NewInstallPage() {
   return (
     <div className="flex flex-col gap-8 p-8">
       <div className="flex items-center gap-2">
-        <Link
-          className="cursor-pointer rounded p-1 text-neutral-400 transition hover:bg-white/10 hover:text-neutral-200"
-          to="/installs"
-        >
+        <Link className="btn btn-ghost p-1" to="/installs">
           <ArrowLeftIcon size={20} />
         </Link>
         <h1 className="text-2xl font-semibold">Install Godot Editor</h1>
       </div>
-      <div className="flex flex-col gap-4">
-        {versions?.map((version) => (
-          <VersionCard key={version.name} version={version} />
-        ))}
+      <div>
+        <ul className="list gap-4">
+          {versions?.map((version) => (
+            <li key={version.name}>
+              <VersionCard version={version} />
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
@@ -36,14 +37,14 @@ function VersionCard({ version }: { version: Version }) {
   const navigate = useNavigate();
 
   return (
-    <div className="flex items-center gap-2 rounded border border-white/10 bg-neutral-800 p-4">
+    <div className="card flex items-center gap-2 p-4">
       <div className="flex flex-1 items-center gap-2">
         <img className="size-8" src="/icon.svg" />
         <div className="font-semibold">Godot {version.name}</div>
       </div>
       <div className="flex items-center gap-2">
         <a
-          className="flex cursor-pointer items-center gap-1 text-sm text-neutral-400 transition hover:text-neutral-200"
+          className="btn btn-link text-sm"
           href={version.release_notes}
           target="_blank"
         >
@@ -51,7 +52,7 @@ function VersionCard({ version }: { version: Version }) {
           <ExternalLinkIcon size={16} />
         </a>
         <button
-          className="cursor-pointer rounded bg-blue-500 px-2 py-1 font-semibold transition hover:bg-blue-600 disabled:cursor-default disabled:bg-neutral-800"
+          className="btn btn-primary"
           disabled={version.installed}
           onClick={() => {
             install(version.name, version.flavor).catch((e) =>
